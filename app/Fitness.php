51e53d7c4d81f2b877b0e84bd0ce0bdd300a1310
ardiansyah-sweng <ardiansyah@tif.uad.ac.id>
2021-12-09 16:10:57 +0700

    Add selection fitChromosomes[]

--- app/Fitness.php
+++ app/Fitness.php
@@ -14,33 +14,20 @@ class Fitness
         $this->maxBudget = $maxBudget;
     }
 
-    function assigningGenWithPrice(array $chromosomes): array
+    function getAmount(array $chromosomes): float
     {
-        $binary1Genes = [];
         $catalogue = new Catalogue;
         $products = $catalogue->getAllProducts();
 
+        $amount = 0;
         if (count(array_unique($chromosomes)) > 1) {
             foreach ($chromosomes as $key => $gen) {
                 if ($gen === 1) {
-                    $binary1Genes[] = [
-                        'selectedGen' => $key,
-                        'price' => $products[$key]['item_price']
-                    ];
+                    $amount += $products[$key]['item_price'];
                 }
             }
         }
-
-        if (count(array_unique($chromosomes)) === 1) {
-            foreach ($chromosomes as $key => $gen) {
-                $binary1Genes[] = [
-                    'selectedGen' => 0,
-                    'price' => 0
-                ];
-            }
-        }
-
-        return $binary1Genes;
+        return $amount;
     }
 
     function calculateFitnessValue(): array
---
+++ app/Selection.php
@@ -0,0 +1,45 @@
+<?php
+
+interface Selection
+{
+    function selectingSelection($population, $maxBudget);
+}
+
+class Elitism implements Selection
+{
+    public $crossoverOffsprings;
+
+    function createTemporaryPopulation($population)
+    {
+        foreach ($this->crossoverOffsprings as $offspring) {
+            $population[] = $offspring;
+        }
+        return $population;
+    }
+
+    function selectingSelection($population, $maxBudget)
+    {
+        $tempPopulation = $this->createTemporaryPopulation($population);
+        $fitness = new Fitness($population, $maxBudget);
+
+        foreach ($tempPopulation as $chromosomes){
+            $amount = $fitness->getAmount($chromosomes);
+            if ($amount <= $maxBudget){
+                $fitChromosomes[] = $chromosomes;
+            }
+        }
+        return $fitChromosomes;
+    }
+}
+
+class SelectionFactory
+{
+    function initializeSelectionFactory($selectionType, $population, $crossoverOffsprings, $maxBudget)
+    {
+        if ($selectionType === 'elitism'){
+            $elitism = new Elitism;
+            $elitism->crossoverOffsprings = $crossoverOffsprings;
+            return $elitism->selectingSelection($population, $maxBudget);
+        }
+    }
+}
